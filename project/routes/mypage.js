d044b9d81ebef78453e94bff7ad25d89211a33a5
Jonghyeok Park <akindo19@gmail.com>
2016-06-06 00:19:24 +0900

    mypage uncompleted

--- project/routes/mypage.js
+++ project/routes/mypage.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/mypage', function(req, res, next) {
-  //res.send('respond with a resource');
-  res.render('mypage', { title: 'mypage' });
+var mysql = require('mysql');
+var dbconfig = require('../config/database');
+var connection = mysql.createConnection(dbconfig.connection);
 
+connection.query('USE ' + dbconfig.database);
+
+router.get('/mypage', isLoggedIn, function(req, res) {
+
+  connection.query("SELECT * FROM friends WHERE user1 = ?",[req.user['email']], function(err, rows) {
+      if (err)
+          return done(err);
+      if (rows.length) {
+        console.log("[mypage] 친구있음");
+        res.render('mypage', {
+      user: req.user, result: rows});
+      }
+      else
+      {
+        console.log("[mypage] 친구없음");
+        res.render('mypage', {
+      user: req.user, result: null});
+      }
+    });
 });
 
 module.exports = router;
