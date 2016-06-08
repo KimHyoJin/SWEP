4ecad7d522401170efeb0e2dc379bc3d7e969d75
Jonghyeok Park <akindo19@gmail.com>
2016-06-08 21:01:10 +0900

    app.js basic setting uncompleted

--- project/app.js
+++ project/app.js
@@ -35,11 +35,9 @@ var mypage = require('./routes/mypage.js');
 // Video Share
 var mediashare = require('./routes/mediashare.js');
 
-var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs'); // set up ejs for templating
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -49,8 +47,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); // set up ejs for templating
 
 app.use('/', login);
 app.get('/mediaupload', mediaupload);
@@ -60,14 +56,14 @@ app.get('/mediashare', mediashare);
 
 // required for passport
 app.use(session({
-	secret: 'vidyapathaisalwaysrunning',
-	resave: true,
-	saveUninitialized: true
+        secret: 'vidyapathaisalwaysrunning',
+        resave: true,
+        saveUninitialized: true
  } )); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-
+app.use(busboy());
 
 
 // catch 404 and forward to error handler
