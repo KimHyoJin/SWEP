727d92ba9a9911bcbe4e772fd51aaf0809069e11
KimHyoJin <lawliet1212@naver.com>
2016-06-10 02:00:34 +0900

    setup database module in mediashare.js

--- project/routes/mediashare.js
+++ project/routes/mediashare.js
@@ -1,57 +1,14 @@
 var express = require('express');
-var router = express.Router();
+var flash    = require('connect-flash');
+var passport = require('passport');
+var mysql = require('mysql');
+var dbconfig = require('../config/database');
+var fs = require('fs');
 
-/* GET home page. */
-router.get('/mediashare', function(req, res, next) {
-//  res.render('mediashare', { title: 'mediashare' });
+module.exports = (function() {
+    var router = express.Router();
+    var connection  = mysql.createConnection(dbconfig.connection);
+    connection.query('USE ' + dbconfig.database);
 
-  	var admin = "admin@root";
-  	var friends ="";
+})();
 
-  	connection.query("SELECT user2 FROM friends WHERE user1= ?",[req.user['email']], function(err, rows) {
-      if (err)
-         return done(err);
-      if (rows.length) {
-      	friends = rows;
-      	console.log("[mediashare] 친구 있음");
-
-      	connection.query("SELECT * FROM video WHERE isStore = ?",[admin], function(err, rows) {
-  	    if (err)
-  	        return done(err);
-  	    if (rows.length) {
-  	    	console.log("[mediashare] 비디오 있음");
-
-
-  	    	res.render('mediashare', {
-  				user: req.user, result: rows, myfriend: friends});
-  	    }
-  	    else
-  	    {
-  	    	console.log("[mediashare] 비디오 없음");
-  		    connection.query("SELECT * FROM video WHERE isStore = ?",[admin], function(err, rows) {
-  		    if (err)
-  		        return done(err);
-  		    if (rows.length) {
-  		    	console.log("[mediashare] 비디오 있음");
-
-  		    	res.render('mediashare', {
-  					user: req.user, result: null, myfriend: friends});
-  		    }
-  	    });
-  		}
-  		});
-      }
-      else
-      {
-      	console.log("[mediashare] 친구 없음");
-      	res.render('mypage', {
-  			user: req.user, result: null});
-      }
-  	});
-
-});
-
-
-
-
-module.exports = router;
