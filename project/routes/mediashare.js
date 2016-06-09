96d50a038788dd3509ebf3566ecdab38e28fb23c
KimHyoJin <lawliet1212@naver.com>
2016-06-10 02:33:36 +0900

    Redirecting to chatroom in mediashare.js

--- project/routes/mediashare.js
+++ project/routes/mediashare.js
@@ -10,6 +10,24 @@ module.exports = (function() {
     var connection  = mysql.createConnection(dbconfig.connection);
     connection.query('USE ' + dbconfig.database);
 
+
+    router.get('/chatroom', isLoggedIn, function(req, res){
+      connection.query("SELECT count(*) as `count`, channel, video FROM chat WHERE receiver = ? order by chatid desc",[ req.user['email'] ], function(err, rows) {
+        var channel = "";
+        var video= "";
+
+        if (rows[0].count)
+        {
+          channel = rows[0].channel;
+          video = rows[0].video;
+        }
+        if (!video)
+          video = req.query.video;
+        console.log(rows[0].count, rows[0].channel, rows[0].video);
+        res.render('chatroom', {user: req.user, channel: channel, video: video});
+      });
+    });
+
 })();
 
 function isLoggedIn(req, res, next) {
