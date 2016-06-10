136fd1d77dc4de3111a4fe003ccf41466600666e
YongwooCho <jywdyddn@naver.com>
2016-06-10 10:31:58 +0900

    hehe

--- project/routes/login.js
+++ project/routes/login.js
@@ -12,4 +12,15 @@ module.exports = (function() {
 				    	router.get('/', function(req, res) {
 								res.render('login', { message: req.flash('signupMessage'), message2: req.flash('loginMessage')} );
 									});
+// logout
+router.get('/logout', function(req, res) {
+	req.logout();
+	connection.query("DELETE FROM chat",function(err, rows){
+		if(err)
+		        {console.log("DELETE CHAT ERROR");}
+	        else
+		        {console.log("DELETE CHAT SUCCESS");}
+	        });
+	        res.redirect('/');
+});
 
